3d1323cc6b9f5f40891a9ac40d5de469e04176a1
chrisngili <ngilichristopher@gmail.com>
2025-03-06 19:13:02 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+// Code your solution in this file
+const returnFirstTwoDrivers= function([Antonia, Nuru, Amari, Mo]){
+    return ['Antonia', 'Nuru'];
+}
+const returnLastTwoDrivers = function([Antonia, Nuru, Amari, Mo]){
+    return ['Amari', 'Mo'];
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = function(numero){
+    return function(fare){
+        return fare*numero
+    }
+}
+
+const fareDoubler = function(fare){
+    return fare*2
+}
+const fareTripler = function(fare){
+    return fare*3
+}
+
+const selectDifferentDrivers = function ([Antonia, Nuru, Amari, Mo],returnLastTwoDrivers){
+    return returnLastTwoDrivers([Antonia, Nuru, Amari, Mo])
+}
